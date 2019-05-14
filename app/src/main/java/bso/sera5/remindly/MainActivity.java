bc333544fba9f0bbd24313199eeffbb9a01b10aa
FikZet701 <fikrydz701gmail.com>
2019-05-14 07:38:09 +0700

    pertemuan 1

---
+++ app/src/main/java/bso/sera5/remindly/MainActivity.java
@@ -0,0 +1,14 @@
+package bso.sera5.remindly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/bso/sera5/remindly/ReminderAddActivity.java
@@ -0,0 +1,14 @@
+package bso.sera5.remindly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReminderAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder_add);
+    }
+}
